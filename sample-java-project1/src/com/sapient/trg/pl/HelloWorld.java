bd49d917c377a39f67ec5b78a7baf7e4db66961a
Amlan Sahoo <sahoo.amlanranjan@gmail.com>
2021-06-21 11:40:29 +0530

    my initial commit

---
+++ sample-java-project1/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.sapient.trg.pl;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Exit World");
+	}
+
+}
